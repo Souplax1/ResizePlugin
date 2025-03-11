14c01efaa2bc8d6fd5f4de5df48c303770c25c78
yeezy <yeezy@DESKTOP-SIQSJQP>
2025-03-11 23:56:38 +0200

    fix

--- Class1.cs
+++ Class1.cs
@@ -1,4 +1,4 @@
-using CounterStrikeSharp.API;
+﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Admin;
